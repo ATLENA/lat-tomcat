13c06ba0b662f527b4298f1dec76b2dab1410da3
서세진 책임/SWA Innovation팀 <sjsj@lgcns.com>
2023-05-16 00:38:45 +0900

    Tomcat engine and template packaging #11
    enlena -> management

--- was-oss/tomcat/9.0/template/base/start.sh
+++ was-oss/tomcat/9.0/template/base/start.sh
@@ -4,5 +4,5 @@ SCRIPTPATH=`cd $(dirname $0) ; pwd -P`
 
 . ${SCRIPTPATH}/env.sh
 
-${LAT_HOME}/enlena/*RELEASE/bin/latctl.sh start tomcat ${INSTANCE_ID}
+${LAT_HOME}/management/*RELEASE/bin/latctl.sh start tomcat ${INSTANCE_ID}
 
