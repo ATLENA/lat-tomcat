308b9aa916ce57056e2f8531ccd35b9336ff8b46
moonct <tokybob@gmail.com>
2025-02-02 17:23:52 +0900

    feat: update stop.sh

--- was-oss/tomcat/9.0/template/9.0.87/base/stop.sh
+++ was-oss/tomcat/9.0/template/9.0.87/base/stop.sh
@@ -4,5 +4,5 @@ SCRIPTPATH=`cd $(dirname $0) ; pwd -P`
 
 . ${SCRIPTPATH}/env.sh
 
-${LAT_HOME}/management/latctl/bin/latctl.sh stop tomcat ${INSTANCE_ID}
+${LAT_HOME}/latctl.sh stop tomcat ${INSTANCE_ID}
 
