37121601dacefba09cac45ccad5cdd0dfde570a6
moonct <tokybob@gmail.com>
2025-02-02 17:23:34 +0900

    feat: update start.sh

--- was-oss/tomcat/9.0/template/9.0.87/base/start.sh
+++ was-oss/tomcat/9.0/template/9.0.87/base/start.sh
@@ -4,5 +4,5 @@ SCRIPTPATH=`cd $(dirname $0) ; pwd -P`
 
 . ${SCRIPTPATH}/env.sh
 
-${LAT_HOME}/management/latctl/bin/latctl.sh start tomcat ${INSTANCE_ID}
+${LAT_HOME}/latctl.sh start tomcat ${INSTANCE_ID}
 
